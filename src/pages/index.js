af6b81468806f4299bbbfc2bd4ddaa589ad8ec35
Zubair N. Ahmed <zubair.ahmed@alumni.stanford.edu>
2018-08-30 13:57:30 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Hello everyone, my name is Zubair Ahmed. I'm a software engineer who wants to help build a better world.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
